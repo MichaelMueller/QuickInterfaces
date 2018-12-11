a631b2ce57e147968e47aca84e1089edb7ad55e8
mueller <mueller@mbits.info>
2018-12-11 11:33:18 +0100

    Checkpoint commit as of 2018-12-11-11-33-14.

--- src/File.php
+++ src/File.php
@@ -12,49 +12,54 @@ interface File
   /**
    * @return string the file extension. no dot. if this is a dir null will be returned
    */
-  function getExtension();
+  function getExtension ();
 
   /**
    * @return string the file basename, i.e. the filename without extension
    */
-  function getBasename();
+  function getBasename ();
 
   /**
    * @return string the file name
    */
-  function getFileName();
+  function getFileName ();
 
   /**
    * @return string the file's path
    */
-  function getPath();
+  function getPath ();
 
   /**
    * @return int the size of the file
    */
-  function getSize();
+  function getSize ();
+
+  /**
+   * @return bool
+   */
+  function exists ();
 
   /**
    * @return string the file's path
    */
-  function getParentDir();
+  function getParentDir ();
 
   /**
    * @return bool if this is a dir
    */
-  function isDir();
+  function isDir ();
 
   /**
    * Get the contents of a file using file locking
    * @param string $FilePath
    * @return string the data contained or null if file does not exist
    */
-  function readContents();
+  function readContents ();
 
   /**
    * Writes data to a file (overwrite or create) with an exclusive lock
    * @param string $filePath
    * @param string $data
    */
-  function writeContents($data);
+  function writeContents ( $data );
 }
